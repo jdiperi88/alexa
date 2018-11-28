41a637ca00966decadcb384e0a844347a2220075
jdiperi88 <jdiperi88@yahoo.com>
2018-11-28 10:49:24 -0500

    added another intent to greeting function

--- greeting/index.js
+++ greeting/index.js
@@ -5,7 +5,7 @@ var Alexa = require("alexa-sdk");
 var handlers = {
 	LaunchRequest: function() {
 		this.response
-			.speak("What is your favorite progamming language?")
+			.speak("Hello, what do you think is the world's most popular sport?")
 			.listen(
 				"I am sorry, I didn't get that. What is your favorite programming language?"
 			);
@@ -13,11 +13,46 @@ var handlers = {
 	},
 
 	LanguageIntent: function() {
-		this.response.speak("That is a great language.?");
+		this.response.speak(
+			" Hello, what do you think is the world's most popular sport?"
+		);
 		this.emit(":responseReady");
 	},
+	// LanguageIntent: function() {
+	// 	this.response.speak(
+	// 		" Hello, what do you think is the world's most popular sport?"
+	// 	);
+	// 	this.emit(":responseReady");
+	// },
 	OrderIntent: function() {
 		this.response.speak(`Alrighty, good bye. `);
+		this.emit(":responseReady");
+	},
+	MostPopularSportIntent: function() {
+		console.log(this.event.request.intent.slots.sport.value);
+		var worldSport = this.event.request.intent.slots.sport.value;
+		if (worldSport === "soccer") {
+			this.response.speak("Correct! Soccer is the world's most popular sport.");
+		} else {
+			this.response.speak(
+				"You guessed that " +
+					worldSport +
+					" is the most popular. Actually, soccer is the world's most popular sport"
+			);
+		}
+		this.emit(":responseReady");
+	},
+	ChildIntent: function() {
+		var favoriteChild = this.event.request.intent.slots.child.value;
+		if (favoriteChild === "joey") {
+			this.response.speak("Correct! Joey is Maura Casey's favorite son");
+		} else {
+			this.response.speak(
+				"You guessed that Maura Casey's favorite son is " +
+					favoriteChild +
+					". Actually, Joey is the Maura's favorite"
+			);
+		}
 		this.emit(":responseReady");
 	}
 };
